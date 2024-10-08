8983a5a41df0b757470ba04e4dbe1fa28ceed872
v_mar <v.martinek@mail.de>
2024-10-08 14:45:00 +0200

    adapted recursive complexity -> now simpler and only probibits nesting -> nesting is everything except + and -. A more nuanced complexity for thermo EOS / intuitive complexity is required

--- src/specific_measures.jl
+++ src/specific_measures.jl
@@ -5,38 +5,32 @@
     - personally (VM), I think variables deserve less complexity than parameters
     - which complexity to use: { (lef * rig) + 1.0 } vs. { 2.0^(lef + rig) }
 """
-global const recursive_compl_dict = Dict(
-        :+       => (lef, rig) -> lef + rig,
-        :-       => (lef, rig) -> lef + rig,
-        :*       => (lef, rig) -> (lef * rig) + 1.0,
-        :/       => (lef, rig) -> (lef * rig) + 1.0,
-        :^       => (lef, rig) -> (lef * rig) + 1.0, # 2.0^(lef + rig) ?
-        :pow_abs => (lef, rig) -> (lef * rig) + 1.0, # 2.0^(lef + rig) ?
-)
-
 function recursive_compl(node::Node, ops)
     if node.ari == 2
         compl_lef = recursive_compl(node.lef, ops)
         compl_rig = recursive_compl(node.rig, ops)
-        cur_op = Symbol(ops.binops[node.ind])
 
-        compl = recursive_compl_dict[cur_op](compl_lef, compl_rig)
+        if ops.binops[node.ind] in (+, -)
+            compl = compl_lef + compl_rig + 1
+        else
+            compl = (compl_lef * compl_rig) + 1
+        end
 
     elseif node.ari == 1
         compl_lef = recursive_compl(node.lef, ops)
-        compl = 2.0^compl_lef
+        compl = compl_lef^2 + 1
 
     elseif node.ari == 0
-        compl = 2.0
+        compl = 1.5
 
     elseif node.ari == -1
-        compl = 1.0
+        compl = 2.0
     end
 
     return min(floatmax(), compl)
 end
 
-# more nuanced recursive_compl
+# # more nuanced recursive_compl
 # global const recursive_compl_dict = Dict(
 #     :+           => (lef, rig) -> lef + rig,
 #     :-           => (lef, rig) -> lef + rig,
@@ -44,8 +38,8 @@ end
 #     :/           => (lef, rig) -> (1.5 * lef + 2 * rig) + 1.0,
 #     :^           => (lef, rig) -> (1.5 * lef + 3 * rig) + 1.0,
 #     :pow_abs     => (lef, rig) -> (1.5 * lef + 3 * rig) + 1.0,
-#     :pow_integer => (lef, rig) -> (1.5 * lef          ) + 1.0,
-#     :pow_float   => (lef, rig) -> (1.5 * lef + 1.0    ) + 1.0,
+#     # :pow_integer => (lef, rig) -> (1.5 * lef          ) + 1.0,
+#     # :pow_float   => (lef, rig) -> (1.5 * lef + 1.0    ) + 1.0,
 # )
 #
 # function recursive_compl(node::Node, ops)
