28f1437c7b202fae63f990f5fad895593c5385e9
v_mar <v.martinek@mail.de>
2025-03-03 14:17:05 +0100

    reverted eval change

--- src/node_n_eval_n_utilities.jl
+++ src/node_n_eval_n_utilities.jl
@@ -55,9 +55,9 @@ end
     For now, the following evalulations are prevented:
     log(x <= 0), pow(x < 0, y), x / 0
 """
-function eval_equation(node::Node{T}, data::AbstractArray, ops::Options)::Tuple{Union{AbstractArray{T}, T},Bool} where {T <: Real}
+function eval_equation(node::Node{T}, data::AbstractArray, ops::Options)::Tuple{AbstractArray{T}, Bool} where {T <: Real}
     if node.ari == -1
-        return node.val, true
+        return fill(node.val, length(data[1])), true
     elseif node.ari == 0
         return data[node.ind], true
     elseif node.ari == 1
