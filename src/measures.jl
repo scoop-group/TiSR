02642fde1d4dc5baafe49372322a2482a100be3a
v_mar <v.martinek@mail.de>
2025-03-03 13:17:53 +0100

    commented the recursive_compl -> project for later

--- src/measures.jl
+++ src/measures.jl
@@ -119,39 +119,39 @@ end
 """
 get_measure_compl(prediction, target, node, ops)           = count_nodes(node)
 get_measure_weighted_compl(prediction, target, node, ops)  = get_weighted_compl(node, ops)
-get_measure_recursive_compl(prediction, target, node, ops) = recursive_compl(node, ops)
+# get_measure_recursive_compl(prediction, target, node, ops) = recursive_compl(node, ops)
 get_measure_n_params(prediction, target, node, ops)        = length(list_of_param_nodes(node))
 
-""" Recursive complexity according to the dissertation of Kommenda 2018 except for the rule
-    for ^, which was not addressed.
-    Open TODO/caveats:
-    - personally (VM), I think variables deserve less complexity than parameters
-    - which complexity to use: { (lef * rig) + 1.0 } vs. { 2.0^(lef + rig) }
-"""
-function recursive_compl(node::Node, ops)
-    if node.ari == 2
-        compl_lef = recursive_compl(node.lef, ops)
-        compl_rig = recursive_compl(node.rig, ops)
-
-        if ops.binops[node.ind] in (+, -)
-            compl = compl_lef + compl_rig + 1
-        else
-            compl = (compl_lef * compl_rig) + 1
-        end
-
-    elseif node.ari == 1
-        compl_lef = recursive_compl(node.lef, ops)
-        compl = compl_lef^2 + 1
-
-    elseif node.ari == 0
-        compl = 1.5
-
-    elseif node.ari == -1
-        compl = 2.0
-    end
-
-    return min(floatmax(), compl)
-end
+# """ Recursive complexity according to the dissertation of Kommenda 2018 except for the rule
+#     for ^, which was not addressed.
+#     Open TODO/caveats:
+#     - personally (VM), I think variables deserve less complexity than parameters
+#     - which complexity to use: { (lef * rig) + 1.0 } vs. { 2.0^(lef + rig) }
+# """
+# function recursive_compl(node::Node, ops)
+#     if node.ari == 2
+#         compl_lef = recursive_compl(node.lef, ops)
+#         compl_rig = recursive_compl(node.rig, ops)
+#
+#         if ops.binops[node.ind] in (+, -)
+#             compl = compl_lef + compl_rig + 1
+#         else
+#             compl = (compl_lef * compl_rig) + 1
+#         end
+#
+#     elseif node.ari == 1
+#         compl_lef = recursive_compl(node.lef, ops)
+#         compl = compl_lef^2 + 1
+#
+#     elseif node.ari == 0
+#         compl = 1.5
+#
+#     elseif node.ari == -1
+#         compl = 2.0
+#     end
+#
+#     return min(floatmax(), compl)
+# end
 
 # # more nuanced recursive_compl
 # global const recursive_compl_dict = Dict(
